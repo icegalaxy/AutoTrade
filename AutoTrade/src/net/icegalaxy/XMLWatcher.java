a0a132730b46b5065fecaa9e4e014131a8bad027
icegalaxy@Mac mini <joecheung0@gmail.com>
2018-03-13 11:13:00 +0800

    Fixed readStair in XMLWatcher

--- AutoTrade/src/net/icegalaxy/XMLWatcher.java
+++ AutoTrade/src/net/icegalaxy/XMLWatcher.java
@@ -253,9 +253,9 @@ public class XMLWatcher implements Runnable
 			st.value = sc2.nextDouble();
 			st.cutLoss = sc2.nextDouble();
 			st.buying = sc2.nextBoolean();
-			st.cutLoss = sc2.nextDouble();
+			st.refLow = sc2.nextDouble();
 			st.selling = sc2.nextBoolean();
-			st.cutLoss = sc2.nextDouble();
+			st.refHigh = sc2.nextDouble();
 			st.reActivateTime = sc2.nextInt();
 			st.shutdown = sc2.nextBoolean();
 			
