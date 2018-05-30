a2721e636053c04f4333dc58550edcd9d8cf0a8a
IceGalaxy@MacAir <joecheung0@gmail.com>
2018-05-30 09:01:16 +0800

    HSIM8

--- AutoTrade/src/net/icegalaxy/SPApi.java
+++ AutoTrade/src/net/icegalaxy/SPApi.java
@@ -21,8 +21,8 @@ public class SPApi
 {
 	static int counter;
 	static long status = 0;
-	static byte[] product = getBytes("MHIK8", 16);
-	static byte[] watchingProduct = getBytes("HSIK8", 16);
+	static byte[] product = getBytes("MHIM8", 16);
+	static byte[] watchingProduct = getBytes("HSIM8", 16);
 	
 //	static ArrayList<SPApiOrder> orders = new ArrayList<SPApiOrder>();
 
