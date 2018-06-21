f5a3bd6581df65af8837cf4ef133ee71e51a8ec8
joech <joech@DESKTOP-2PLH22R>
2018-06-21 09:09:23 +0800

    Disabled XMLWatcher

--- AutoTrade/src/net/icegalaxy/XMLWatcher.java
+++ AutoTrade/src/net/icegalaxy/XMLWatcher.java
@@ -140,14 +140,14 @@ public class XMLWatcher implements Runnable
 		}
 
 		// reset XMLWatcher
-		XMLWatcher.updateIntraDayXML("stair", "0");
-		XMLWatcher.updateIntraDayXML("cutLoss", "0");
-		XMLWatcher.updateIntraDayXML("stopEarn", "0");
-		XMLWatcher.updateIntraDayXML("rangeResist", "0");
-		XMLWatcher.updateIntraDayXML("rangeSupport", "0");
-		XMLWatcher.updateIntraDayXML("SAR", "0");
-		XMLWatcher.updateIntraDayXML("buying", "false");
-		XMLWatcher.updateIntraDayXML("selling", "false");
+//		XMLWatcher.updateIntraDayXML("stair", "0");
+//		XMLWatcher.updateIntraDayXML("cutLoss", "0");
+//		XMLWatcher.updateIntraDayXML("stopEarn", "0");
+//		XMLWatcher.updateIntraDayXML("rangeResist", "0");
+//		XMLWatcher.updateIntraDayXML("rangeSupport", "0");
+//		XMLWatcher.updateIntraDayXML("SAR", "0");
+//		XMLWatcher.updateIntraDayXML("buying", "false");
+//		XMLWatcher.updateIntraDayXML("selling", "false");
 
 		try
 		{
