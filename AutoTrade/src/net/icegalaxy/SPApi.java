b2d82988e37398dd3a3dc652f4dfdf01bb01ee16
joecheung0 <joecheung0@window.asia-east2-a.c.sptrade.internal>
2019-03-12 06:47:24 +0800

    HSIH9

--- AutoTrade/src/net/icegalaxy/SPApi.java
+++ AutoTrade/src/net/icegalaxy/SPApi.java
@@ -21,8 +21,8 @@ public class SPApi
 {
 	static int counter;
 	static long status = 0;
-	static byte[] product = getBytes("MHIN8", 16);
-	static byte[] watchingProduct = getBytes("HSIN8", 16);
+	static byte[] product = getBytes("MHIH9", 16);
+	static byte[] watchingProduct = getBytes("HSIH9", 16);
 	
 //	static ArrayList<SPApiOrder> orders = new ArrayList<SPApiOrder>();
 
